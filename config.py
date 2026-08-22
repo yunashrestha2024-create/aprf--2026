11b95cda2f9eccc1b4a909f95fb9cd3eeb9c0d93
yunashrestha2024-create <yunashrestha2024@gmail.com>
2026-08-22 16:03:01 +0545

    Add configuration for Neo4j database connection

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+NEO4J_URI = "neo4j+s://9f649b76.databases.neo4j.io"
+NEO4J_USER = "9f649b76"  # IMPORTANT: Use the instance ID, NOT 'neo4j'!
+NEO4J_PASSWORD = "HYZfC5hrz9STd7gn2Y37KLsVmKW5hJdhs_IfQTypgNk"
+NEO4J_DATABASE = "9f649b76"
